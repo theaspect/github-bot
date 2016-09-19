903c66d5906c4170c14df3a278cc3ca1a6c8ee8c
Constantine Linnick <theaspect@gmail.com>
2016-09-19 23:15:05 +0300

    Add driver param

--- src/main/java/me/blzr/github/App.java
+++ src/main/java/me/blzr/github/App.java
@@ -1,12 +1,11 @@
 package me.blzr.github;
 
 import com.google.common.base.Preconditions;
-import com.mchange.v2.c3p0.DataSources;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.telegram.telegrambots.TelegramBotsApi;
 
-import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -20,6 +19,7 @@ public class App {
     private final String username;
     private final String token;
     private final String port;
+    private final String driver;
 
     public static void main(String[] args) {
         try {
@@ -31,24 +31,38 @@ public class App {
 
     public App() throws IOException {
         Properties prop = getProperties();
-
+        //DB
+        this.driver = prop.getProperty("driver", System.getenv("DRIVER"));
         this.url = prop.getProperty("url", System.getenv("URL"));
         this.user = prop.getProperty("user", System.getenv("USER"));
         this.password = prop.getProperty("password", System.getenv("PASSWORD"));
+        // Telegram
         this.username = prop.getProperty("username", System.getenv("USERNAME"));
         this.token = prop.getProperty("token", System.getenv("TOKEN"));
+        // Server
         this.port = prop.getProperty("port", System.getenv("PORT"));
 
+        Preconditions.checkNotNull(driver, "DRIVER should not be null");
         Preconditions.checkNotNull(url, "URL should not be null");
         Preconditions.checkNotNull(user, "USER should not be null");
         Preconditions.checkNotNull(password, "PASSWORD should not be null");
         Preconditions.checkNotNull(username, "USERNAME should not be null");
         Preconditions.checkNotNull(token, "TOKEN should not be null");
         Preconditions.checkNotNull(port, "PORT should not be null");
+
+        log.debug("Will connect to {}@{} using", user, url, driver);
+        log.debug("Will listen {}", port);
     }
 
     private Properties getProperties() throws IOException {
         log.debug("Reading properties");
+
+        // TRACE c3p0
+        //Properties p = new Properties(System.getProperties());
+        //p.put("com.mchange.v2.log.MLog", "com.mchange.v2.log.FallbackMLog");
+        //p.put("com.mchange.v2.log.FallbackMLog.DEFAULT_CUTOFF_LEVEL", "ALL"); // Off or any other level
+        //System.setProperties(p);
+
         Properties prop = new Properties();
         InputStream is = App.class.getResourceAsStream("/config.properties");
         if (is != null) {
@@ -58,7 +72,12 @@ public class App {
     }
 
     private void start() throws Exception {
-        final DataSource pool = DataSources.pooledDataSource(DataSources.unpooledDataSource(url, user, password));
+        final ComboPooledDataSource pool = new ComboPooledDataSource();
+        pool.setDriverClass(driver);
+        pool.setJdbcUrl(url);
+        pool.setUser(user);
+        pool.setPassword(password);
+
         final Database database = new Database(pool);
         final Bot bot = new Bot(database, username, token);
         final GitHub gitHub = new GitHub();
