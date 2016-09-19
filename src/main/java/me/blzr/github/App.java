88f5da4af029d264ee030bc1b94d4ac728159e65
Constantine Linnick <theaspect@gmail.com>
2016-09-19 07:04:12 +0300

    Add params

--- src/main/java/me/blzr/github/App.java
+++ src/main/java/me/blzr/github/App.java
@@ -1,9 +1,11 @@
 package me.blzr.github;
 
+import com.google.common.base.Preconditions;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.telegram.telegrambots.TelegramBotsApi;
 
+import java.io.InputStream;
 import java.util.Properties;
 
 public class App {
@@ -12,14 +14,29 @@ public class App {
     public static void main(String[] args) throws Exception {
         log.debug("Reading properties");
         Properties prop = new Properties();
-        prop.load(App.class.getResourceAsStream("/config.properties"));
+        InputStream is = App.class.getResourceAsStream("/config.propertiess");
+        if (is != null) {
+            prop.load(is);
+        }
+
+        final String url = prop.getProperty("url", System.getenv("URL"));
+        final String user = prop.getProperty("user", System.getenv("USER"));
+        final String password = prop.getProperty("password", System.getenv("PASSWORD"));
+        final String username = prop.getProperty("username", System.getenv("USERNAME"));
+        final String token = prop.getProperty("token", System.getenv("TOKEN"));
+
+        Preconditions.checkNotNull(url, "URL should not be null");
+        Preconditions.checkNotNull(user, "USER should not be null");
+        Preconditions.checkNotNull(password, "PASSWORD should not be null");
+        Preconditions.checkNotNull(username, "USERNAME should not be null");
+        Preconditions.checkNotNull(token, "TOKEN should not be null");
 
         // TODO Connection pool
-        Database telegramDb = new Database(prop.getProperty("url"), prop.getProperty("user"), prop.getProperty("password"));
-        Database eventsDb = new Database(prop.getProperty("url"), prop.getProperty("user"), prop.getProperty("password"));
-        Database gitHubDb = new Database(prop.getProperty("url"), prop.getProperty("user"), prop.getProperty("password"));
+        Database telegramDb = new Database(url, user, password);
+        Database eventsDb = new Database(url, user, password);
+        Database gitHubDb = new Database(url, user, password);
 
-        new TelegramBotsApi().registerBot(new Bot(telegramDb, prop.getProperty("username"), prop.getProperty("token")));
+        new TelegramBotsApi().registerBot(new Bot(telegramDb, username, token));
 
         GitHub gitHub = new GitHub();
         new Watcher(eventsDb, gitHubDb, gitHub);
